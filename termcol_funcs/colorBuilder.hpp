16928b79586e883e303636cd82ca67fdbf06b3d6
ohsomike <92176838+ohsomike@users.noreply.github.com>
2023-06-15 00:56:11 -0400

    Add Windows support
    I hope this works .-.

--- termcol_funcs/colorBuilder.hpp
+++ termcol_funcs/colorBuilder.hpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#ifdef _WIN32
+    #include <windows.h>
+#endif
 
 namespace termcol
 {
@@ -12,11 +15,28 @@ namespace termcol
     */
     void colorBuilder(string text, int color, bool isBright, bool isBackground)
     {
-        string escapeCode = "\033[";
-        escapeCode.append(to_string(color));
-        if (isBright == true) { escapeCode.append(";1"); };
-        if (isBackground == true) { escapeCode.append(";7"); };
-        escapeCode.append("m");
-        cout << escapeCode << text << "\033[0m";
+        #ifndef _WIN32
+            string escapeCode = "\033[";
+            escapeCode.append(to_string(color));
+            if (isBright == true) { escapeCode.append(";1"); };
+            if (isBackground == true) { escapeCode.append(";7"); };
+            escapeCode.append("m");
+            cout << escapeCode << text << "\033[0m";
+        #else
+            HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            int calculatedCode = 0;
+            int newColor = color;
+            
+            if (isBright == true) { newColor += 8; };
+            if (isBackground == false) {
+                calculatedCode = newColor;
+            } else {
+                calculatedCode = newColor * 16;
+            }
+
+            SetConsoleTextAttribute(consoleHandle, newColor);
+            cout << text
+            SetConsoleTextAttribute(consoleHandle, 8);
+        #endif
     }
 }
--- termcol_vars.hpp
+++ termcol_vars.hpp
@@ -1,10 +1,21 @@
 enum TerminalColors {
-    BLACK = 30,
-    RED = 31,
-    GREEN = 32,
-    YELLOW = 33,
-    BLUE = 34,
-    MAGENTA = 35,
-    CYAN = 36,
-    WHITE = 37
+    #ifndef _WIN32
+        BLACK = 30,
+        RED = 31,
+        GREEN = 32,
+        YELLOW = 33,
+        BLUE = 34,
+        MAGENTA = 35,
+        CYAN = 36,
+        WHITE = 37
+    #else
+        BLACK = 0,
+        BLUE = 1,
+        GREEN = 2,
+        CYAN = 3,
+        RED = 4,
+        MAGENTA = 5,
+        YELLOW = 6,
+        WHITE = 7
+    #endif
 };
