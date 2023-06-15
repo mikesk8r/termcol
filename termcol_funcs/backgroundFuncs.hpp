97bf44e4a67e32632d73bd6a61678fb3294ac99a
ohsomike <92176838+ohsomike@users.noreply.github.com>
2023-06-15 14:48:28 -0400

    Fix weird syntax errors ;-;

--- termcol_funcs/backgroundFuncs.hpp
+++ termcol_funcs/backgroundFuncs.hpp
@@ -4,80 +4,82 @@
 #include "colorBuilder.hpp"
 #include "../termcol_vars.hpp"
 
+using namespace std;
+
 namespace termcol::background
 {
     void red(string text)
     {
-        colorBuilder(text, TerminalColors::RED, false, true)
+        colorBuilder(text, TerminalColors::RED, false, true);
     }
 
     void green(string text)
     {
-        colorBuilder(text, TerminalColors::GREEN, false, true)
+        colorBuilder(text, TerminalColors::GREEN, false, true);
     }
 
     void yellow(string text)
     {
-        colorBuilder(text, TerminalColors::YELLOW, false, true)
+        colorBuilder(text, TerminalColors::YELLOW, false, true);
     }
 
     void blue(string text)
     {
-        colorBuilder(text, TerminalColors::BLUE, false, true)
+        colorBuilder(text, TerminalColors::BLUE, false, true);
     }
 
     void magenta(string text)
     {
-        colorBuilder(text, TerminalColors::MAGENTA, false, true)
+        colorBuilder(text, TerminalColors::MAGENTA, false, true);
     }
 
     void cyan(string text)
     {
-        colorBuilder(text, TerminalColors::CYAN, false, true)
+        colorBuilder(text, TerminalColors::CYAN, false, true);
     }
 
     void white(string text)
     {
-        colorBuilder(text, TerminalColors::WHITE, false, true)
+        colorBuilder(text, TerminalColors::WHITE, false, true);
     }
 
     void brightBlack(string text)
     {
-        colorBuilder(text, TerminalColors::BLACK, true, true)
+        colorBuilder(text, TerminalColors::BLACK, true, true);
     }
 
     void brightRed(string text)
     {
-        colorBuilder(text, TerminalColors::RED, true, true)
+        colorBuilder(text, TerminalColors::RED, true, true);
     }
 
     void brightGreen(string text)
     {
-        colorBuilder(text, TerminalColors::GREEN, true, true)
+        colorBuilder(text, TerminalColors::GREEN, true, true);
     }
 
     void brightYellow(string text)
     {
-        colorBuilder(text, TerminalColors::YELLOW, true, true)
+        colorBuilder(text, TerminalColors::YELLOW, true, true);
     }
 
     void brightBlue(string text)
     {
-        colorBuilder(text, TerminalColors::BLUE, true, true)
+        colorBuilder(text, TerminalColors::BLUE, true, true);
     }
 
     void brightMagenta(string text)
     {
-        colorBuilder(text, TerminalColors::MAGENTA, true, true)
+        colorBuilder(text, TerminalColors::MAGENTA, true, true);
     }
 
     void brightCyan(string text)
     {
-        colorBuilder(text, TerminalColors::CYAN, true, true)
+        colorBuilder(text, TerminalColors::CYAN, true, true);
     }
 
     void brightWhite(string text)
     {
-        colorBuilder(text, TerminalColors::WHITE, true, true)
+        colorBuilder(text, TerminalColors::WHITE, true, true);
     }
 }
--- termcol_funcs/textFuncs.hpp
+++ termcol_funcs/textFuncs.hpp
@@ -4,80 +4,82 @@
 #include "colorBuilder.hpp"
 #include "../termcol_vars.hpp"
 
-namespace termcol
+using namespace std;
+
+namespace termcol::text
 {
     void red(string text)
     {
-        colorBuilder(text, TerminalColors::RED, false, false)
+        colorBuilder(text, TerminalColors::RED, false, false);
     }
 
     void green(string text)
     {
-        colorBuilder(text, TerminalColors::GREEN, false, false)
+        colorBuilder(text, TerminalColors::GREEN, false, false);
     }
 
     void yellow(string text)
     {
-        colorBuilder(text, TerminalColors::YELLOW, false, false)
+        colorBuilder(text, TerminalColors::YELLOW, false, false);
     }
 
     void blue(string text)
     {
-        colorBuilder(text, TerminalColors::BLUE, false, false)
+        colorBuilder(text, TerminalColors::BLUE, false, false);
     }
 
     void magenta(string text)
     {
-        colorBuilder(text, TerminalColors::MAGENTA, false, false)
+        colorBuilder(text, TerminalColors::MAGENTA, false, false);
     }
 
     void cyan(string text)
     {
-        colorBuilder(text, TerminalColors::CYAN, false, false)
+        colorBuilder(text, TerminalColors::CYAN, false, false);
     }
 
     void white(string text)
     {
-        colorBuilder(text, TerminalColors::WHITE, false, false)
+        colorBuilder(text, TerminalColors::WHITE, false, false);
     }
 
     void brightBlack(string text)
     {
-        colorBuilder(text, TerminalColors::BLACK, true, false)
+        colorBuilder(text, TerminalColors::BLACK, true, false);
     }
 
     void brightRed(string text)
     {
-        colorBuilder(text, TerminalColors::RED, true, false)
+        colorBuilder(text, TerminalColors::RED, true, false);
     }
 
     void brightGreen(string text)
     {
-        colorBuilder(text, TerminalColors::GREEN, true, false)
+        colorBuilder(text, TerminalColors::GREEN, true, false);
     }
 
     void brightYellow(string text)
     {
-        colorBuilder(text, TerminalColors::YELLOW, true, false)
+        colorBuilder(text, TerminalColors::YELLOW, true, false);
     }
 
     void brightBlue(string text)
     {
-        colorBuilder(text, TerminalColors::BLUE, true, false)
+        colorBuilder(text, TerminalColors::BLUE, true, false);
     }
 
     void brightMagenta(string text)
     {
-        colorBuilder(text, TerminalColors::MAGENTA, true, false)
+        colorBuilder(text, TerminalColors::MAGENTA, true, false);
     }
 
     void brightCyan(string text)
     {
-        colorBuilder(text, TerminalColors::CYAN, true, false)
+        colorBuilder(text, TerminalColors::CYAN, true, false);
     }
 
     void brightWhite(string text)
     {
-        colorBuilder(text, TerminalColors::WHITE, true, false)
+        colorBuilder(text, TerminalColors::WHITE, true, false);
     }
 }
